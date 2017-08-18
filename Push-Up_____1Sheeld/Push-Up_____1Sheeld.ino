6100fb2e136377682e152479a2a9daef294d90ef
Bram Schork <Bram@Bram-Air.home>
2017-08-18 18:39:45 -0400

    added pull-up file

---
+++ Push-Up_____1Sheeld/Push-Up_____1Sheeld.ino
@@ -0,0 +1,74 @@
+//power from digital pin
+//MUST CONNECT THE ARDUINO'S GROUND TO THE SNAP CIRCUITS GROUND
+
+
+#define CUSTOM_SETTINGS
+#define INCLUDE_CLOCK_SHIELD
+#define INCLUDE_PROXIMITY_SENSOR_SHIELD
+
+#include <OneSheeld.h>
+
+
+int totalPushUps = 0;
+
+int hour, minute, second, day, month, year;
+
+int alarmPin = 13;
+
+boolean startTime = true;
+  
+void setup() { 
+
+  OneSheeld.begin();
+  
+  Clock.queryDateAndTime();
+
+  pinMode(alarmPin, OUTPUT);
+
+  Serial.begin(115200);
+  
+}
+
+void loop() {
+  
+    
+  hour = Clock.getHours();
+  minute = Clock.getMinutes();
+  second = Clock.getSeconds();
+  day = Clock.getDay();
+  month = Clock.getMonth();
+  year = Clock.getYear();
+
+  if(hour == 11 && minute == 2) {
+
+
+          if(startTime == true) {
+  
+               Serial.print("boolean is true");
+               digitalWrite(alarmPin, HIGH);
+                   } //boolean close
+                
+                startTime = false;
+        
+
+ 
+          if(ProximitySensor.getValue() == 0) {
+          totalPushUps++;
+          Serial.println(totalPushUps);
+          OneSheeld.delay(1300);
+
+
+          
+                    if(totalPushUps == 5) {
+                      digitalWrite(alarmPin, LOW);
+
+
+
+
+                      
+   } //if-statement(totalPushUps)
+  } //if-statement(proximinity)
+ } //if-statement(time) close
+} //void loop close
+
+
